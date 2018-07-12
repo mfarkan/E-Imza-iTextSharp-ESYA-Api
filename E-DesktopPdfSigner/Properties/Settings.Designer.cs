0b7b850dec0a06f11e1bf772b6caad9f801472f8
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2018-07-12 22:05:16 +0300

    Solution Name fix.

--- E-DesktopPdfSigner/Properties/Settings.Designer.cs
+++ E-DesktopPdfSigner/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PayFlex.Smartbox.EDesktopPdfSigner.Properties {
+namespace EDesktopPdfSigner.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
