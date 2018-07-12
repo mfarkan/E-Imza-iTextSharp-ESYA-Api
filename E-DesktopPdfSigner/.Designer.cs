41f09e8e4a7ed257ee087971d1aac3b84f1a5805
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2018-07-12 22:02:48 +0300

    Solution name fix.

--- E-DesktopPdfSigner/.Designer.cs
+++ E-DesktopPdfSigner/.Designer.cs
@@ -103,6 +103,7 @@
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "PDF İmzala";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
